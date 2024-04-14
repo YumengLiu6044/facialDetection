edc8a1dd32d8aaddf7f7bda4e7894e176c98c4ed
Yumeng Liu <yumel13@uci.edu>
2024-04-14 13:47:38 -0700

    Added project files

---
+++ detector.py
@@ -0,0 +1,13 @@
+import face_recognition as fr
+import pickle
+
+
+class Detector:
+    def __init__(self, mode='hog'):
+        ...
+
+    def fit(self):
+        ...
+
+    def predict(self):
+        ...
